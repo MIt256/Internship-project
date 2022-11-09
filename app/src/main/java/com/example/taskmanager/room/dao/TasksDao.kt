3c47567c97706d31063b85744c65e638e9b26205
Maks <xxzanko@gmail.com>
2022-11-09 13:10:34 +0300

    add tuple

--- app/src/main/java/com/example/taskmanager/room/dao/TasksDao.kt
+++ app/src/main/java/com/example/taskmanager/room/dao/TasksDao.kt
@@ -1,9 +1,7 @@
 package com.example.taskmanager.room.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.example.taskmanager.room.TaskWithMembersTuple
 import com.example.taskmanager.room.entities.TaskDbEntity
 import com.example.taskmanager.room.entities.UserDbEntity
 import kotlinx.coroutines.flow.Flow
@@ -17,6 +15,13 @@ interface TasksDao {
     @Query("SELECT * FROM users, task_members WHERE users.id = task_members.user_id AND task_members.task_id = :taskId")
     fun getTaskMembers(taskId: String): Flow<List<UserDbEntity?>>
 
+    @Transaction
+    @Query("SELECT * FROM tasks WHERE tasks.id = :taskId")
+    fun getTaskWithMembers(taskId: String): Flow<TaskWithMembersTuple>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addTask(task:TaskDbEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAllTasks(tasks:List<TaskDbEntity>)
 }
