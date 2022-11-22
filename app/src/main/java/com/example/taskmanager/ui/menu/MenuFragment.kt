8b139e5e5bcbb1d5df7a6dc1d7e82574ef352037
Maks <xxzanko@gmail.com>
2022-11-22 19:47:14 +0300

    add main functionality

--- app/src/main/java/com/example/taskmanager/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/menu/MenuFragment.kt
@@ -4,31 +4,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.taskmanager.databinding.FragmentMenuBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MenuFragment : Fragment() {
 
     private lateinit var binding: FragmentMenuBinding
+    private val viewModel: MenuViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(MenuViewModel::class.java)
-
         binding = FragmentMenuBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-////        val textView: TextView = binding.textMenu
-//        dashboardViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-        return root
+        binding.projectRecycler.layoutManager = GridLayoutManager(context, 2)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val projectAdapter = ProjectGridAdapter()
+        binding.projectRecycler.adapter = projectAdapter
+
+        projectAdapter.setOnItemClickListener {
+            Toast.makeText(
+                context,
+                "Not implemented",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        viewModel.projects.observe(viewLifecycleOwner) {
+            projectAdapter.submitList(it)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.currentException.collect {
+                Toast.makeText(
+                    context,
+                    "Error: $it",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        viewModel.getProjectsFromDB()
+
     }
 
 
--- app/src/main/java/com/example/taskmanager/ui/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/taskmanager/ui/menu/MenuViewModel.kt
@@ -3,11 +3,37 @@ package com.example.taskmanager.ui.menu
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskmanager.ui.newTask.entities.Project
+import com.example.taskmanager.ui.task.entities.User
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MenuViewModel : ViewModel() {
+@HiltViewModel
+class MenuViewModel @Inject constructor(private val repository: ProjectsRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is menu Fragment"
+    var currentException = MutableSharedFlow<String>()
+
+    private var _projects = MutableLiveData<List<Project>>()
+    val projects: LiveData<List<Project>> = _projects
+
+    private fun setException(exception: String) =
+        viewModelScope.launch { currentException.emit(exception) }
+
+
+    fun getProjectsFromDB(){
+        viewModelScope.launch {
+            try {
+                repository.getProjects().collect{
+                    _projects.value = it
+                }
+            } catch (exception: Exception) {
+                exception.message?.let { setException(it) }
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
