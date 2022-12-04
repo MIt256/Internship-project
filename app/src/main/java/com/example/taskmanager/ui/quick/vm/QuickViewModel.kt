7cbb334ad0690c78c25347ab6ec5d9c3197b5de2
Maks <xxzanko@gmail.com>
2022-12-04 16:36:16 +0300

    add main files

--- app/src/main/java/com/example/taskmanager/ui/quick/vm/QuickViewModel.kt
+++ app/src/main/java/com/example/taskmanager/ui/quick/vm/QuickViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.taskmanager.ui.quick.vm
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.taskmanager.data.repository.ProfileRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import javax.inject.Inject
 
-class QuickViewModel : ViewModel() {
+@HiltViewModel
+class QuickViewModel @Inject constructor(private val repository: QuickRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is quick Fragment"
-    }
-    val text: LiveData<String> = _text
+    val currentException = MutableSharedFlow<String>(extraBufferCapacity = 1)
+
+    val quicks: LiveData<List<Quick>> = repository.getProfileInfo().catch { it.message?.let { currentException.tryEmit(it) } }.asLiveData()
+
 }
